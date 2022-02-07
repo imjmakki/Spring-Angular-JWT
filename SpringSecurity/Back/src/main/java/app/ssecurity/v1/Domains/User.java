9097a9c95d7e08cec6a220e223abf004b1752595
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-07 14:15:56 +0300

    adding domains

---
+++ SpringSecurity/Back/src/main/java/app/ssecurity/v1/Domains/User.java
@@ -0,0 +1,26 @@
+package app.ssecurity.v1.Domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+}
