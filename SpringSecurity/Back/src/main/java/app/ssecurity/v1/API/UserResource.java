afdc0cf64749f72349af2891514181de7d9c6771
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-07 15:13:03 +0300

    adding api

--- SpringSecurity/Back/src/main/java/app/ssecurity/v1/API/UserResource.java
+++ SpringSecurity/Back/src/main/java/app/ssecurity/v1/API/UserResource.java
@@ -1,13 +1,15 @@
 package app.ssecurity.v1.API;
 
+import app.ssecurity.v1.DTO.UserRoleDTO;
+import app.ssecurity.v1.Domain.Role;
 import app.ssecurity.v1.Domain.User;
 import app.ssecurity.v1.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,23 @@ public class UserResource {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/v1/api/users").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/v1/api/roles").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/roles/addUser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody UserRoleDTO userRoleDTO) {
+        userService.addRoleToUser(userRoleDTO.getUsername(), userRoleDTO.getRoleName());
+        return ResponseEntity.ok().build();
     }
 }
---
+++ SpringSecurity/Back/src/main/java/app/ssecurity/v1/DTO/UserRoleDTO.java
@@ -0,0 +1,10 @@
+package app.ssecurity.v1.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserRoleDTO {
+
+    private String username;
+    private String roleName;
+}
